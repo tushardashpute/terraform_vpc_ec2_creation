9c09de6e8a0a37cf7fa8c08426e2af46d3385401
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2020-11-28 20:12:15 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -11,8 +11,8 @@ variable "aws_access_key" {
  default = "AKIAZVIYFOTWIYPSELNN"
 }
 variable "aws_secret_key" {
- default = "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"
+ default = "****************************"
 }
 variable "vpc_security_group_ids" {
- default = "sg-05e3906a3fb0ce0cb"
+ default = ""****************************""
 }
